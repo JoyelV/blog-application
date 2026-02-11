7882c9dd5488057c7224dd532e3e2305d54f5a64
Joyel <Varghesejoyel71@gmail.com>
2026-02-11 22:18:26 +0530

    fix: correct casing in SearchBar import path

--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Input } from "@/app/Components/ui/input"
+import { Input } from "@/app/components/ui/input"
 import { Search } from "lucide-react"
 import { useRouter, useSearchParams } from "next/navigation"
 import { useEffect, useState } from "react"
